a3c6312183676331934f18033b5a4c3583e76997
zzagir <zaynalov005@mail.ru>
2023-08-01 23:01:52 +0300

    14 urok

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,15 +1,16 @@
-import { prop } from '@typegoose/typegoose'
+import { Ref, prop } from '@typegoose/typegoose'
 import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses'
+import { MovieModel } from 'src/movie/movie.model'
 
 export interface UserModel extends Base {}
 
 export class UserModel extends TimeStamps {
-  @prop({ unique: true })
-  email: string
-  @prop()
-  password: string
-  @prop({ default: false })
-  isAdmin: boolean
-  @prop({ default: [] })
-  favorites?: []
+	@prop({ unique: true })
+	email: string
+	@prop()
+	password: string
+	@prop({ default: false })
+	isAdmin: boolean
+	@prop({ default: [], ref: () => MovieModel })
+	favorites?: Ref<MovieModel>[]
 }
